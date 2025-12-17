640edd46c23197c50770fb32b62257a1aaa0d450
Prabhakar Chaurasia <d2pkprabhu7246@gmail.com>
2025-12-17 22:14:56 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,30 +1,63 @@
-let score ="33abc"
+//let score ="33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let valueInNumber =Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 // "33" => 33
 // "33abc"=>NaN
 //true=> 1; false =>0
 
-let isLoggedIn =""
+//let isLoggedIn =""
 
-let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 // 1=> true; 0=> false
 //""=>false
 //"hitesh"=>true
 
- let someNumber=33
+ //let someNumber=33
 
- let stringNumber =String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+ //let stringNumber =String(someNumber)
+ //console.log(stringNumber);
+ //console.log(typeof stringNumber);
  
- 
+ // ***************Operations ****************
+
+ let value =3
+ let negValue =-value
+ console.log(negValue);
+
+ console.log(10 + 5);   // 15
+console.log(10 - 5);   // 5
+console.log(10 * 5);   // 50
+console.log(10 / 5);   // 2
+console.log(10 % 3);   // 1
+console.log(2 ** 3);   // 8
+
+ let str1 = "hello"
+ let str2 ="Prabhu"
+  let str3 =str1 + str2
+  console.log(str3);
+
+  console.log("1" + 2);
+  console.log(1 +"2");
+  console.log("1" +2 +2 );
+  console.log(1 + 2 +"2");
+  
+  let num1,num2,num3
+
+  num1=num2=num3= 2+2
+
+  let gamecounter =100
+  ++gamecounter;
+  console.log(gamecounter);
+  
+  
+  
+  
