37b6d8ddcfb31c255e91976df4420dbf074d4109
Prabhakar Chaurasia <d2pkprabhu7246@gmail.com>
2025-12-17 21:15:54 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,28 @@
+ "use strict"; // treat all JS code as newer version
+
+ //alert(3+3) // we are using browser
+
+ console.log(3
+    +3
+ ) //code redability should be high
+
+ console.log("Prabhu")
+
+
+ let name = "Prabhu"
+ let age =18
+ let isLoggedIn =false
+
+ //number => 2 to the power 53
+ //bigint
+ //string=>""
+ //boolean => true/false
+ //null => standalone value
+ // undefined =>
+ // symbol=>unique
+
+
+ //object
+
+ console.log(typeof undefined);
+ console.log(typeof null);
