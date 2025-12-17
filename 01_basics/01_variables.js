fec7ba6f9697a808dfefee2548978a2c06289769
Prabhakar Chaurasia <d2pkprabhu7246@gmail.com>
2025-12-17 20:59:50 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountID = 144553
+let accountEmail = "d2pk7246@gmail.com"
+var accountPassword = "12345"
+accountcity = "deoria"
+
+//accountID =2 not allowed//
+
+accountEmail = "abc@gmail.com"
+accountPassword = "12321"
+accountcity = "gorakhpur"
+
+console.log(accountID);
+ let accountstate;
+/*
+perfer not to use var
+ because of issue in block scope and functional scope
+ */
+
+ console.table([accountID,accountEmail,accountPassword,accountstate]);
