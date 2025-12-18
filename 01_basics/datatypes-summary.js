c0e638c3e16d988f1d336f684458bbee54c1a859
Prabhakar Chaurasia <d2pkprabhu7246@gmail.com>
2025-12-18 17:48:17 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,5 +1,7 @@
 //Primitive 
 
+const { useImperativeHandle } = require("react");
+
 //7 types : String, Number , Boolean, null, undefined,symbol,Bigint
 
 const score =100
@@ -32,12 +34,37 @@ const myFunction = function(){
     console.log("Hello World");
 }
 console.log(typeof scoreValue); // number
-console.log(typeof BigInt);    // function
-console.log(typeof outsideTemp);// object
-console.log(typeof isLoggedIn);//boolean
-console.log(typeof userEmail);//undefined
-console.log(typeof id);       //symbol
-console.log(typeof myFunction);// function  
+// console.log(typeof BigInt);    // function
+// console.log(typeof outsideTemp);// object
+// console.log(typeof isLoggedIn);//boolean
+// console.log(typeof userEmail);//undefined
+// console.log(typeof id);       //symbol
+// console.log(typeof myFunction);// function  
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (Primitive), Heap(Non- Primitive)
+
+let myYoutubename ="hiteshchoudharydotcom"
+let anothername =myYoutubename
+anothername = "chaiurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
 
 
 
